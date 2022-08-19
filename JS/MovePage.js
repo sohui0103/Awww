3da968b532d725f627949c21be11b70dd5bb3345
zisoo-choi <zisoo_3@naver.com>
2022-08-19 20:18:59 +0900

    changePage - Link 사용

--- JS/MovePage.js
+++ JS/MovePage.js
@@ -3,10 +3,23 @@ script.src = "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+/*
+console.log(location.href); //현재 페이지 출력
 
+var anchor = document.getElementById("myAnchor");
+var result = anchor.href;
+console.log('result');
+
+        
         $(".HeaderLogo").click(function(){document.location.href='http://127.0.0.1:5500/html/Home.html'})
         $(".MusicTalkBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/MusicTalk.html'})
         $(".PlayListBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/PlayList.html'})
         $(".MyPageBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/MyPage.html'})
         $(".LoginBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/Login.html'})
         $(".SignUpBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/SignUp.html'})
+*/
+
+
+
+
+
